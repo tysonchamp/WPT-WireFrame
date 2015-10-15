7c9834571d6c6822b57435a6782c9a2d2e27bf50
Soumya Mondal <tysonchamp@users.noreply.github.com>
2015-10-15 23:59:05 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -71,20 +71,17 @@ function pagination($pages = '', $range = 4)
 }
 
 
-// wp_nav_menu
+// nav menu start
 
 class themeslug_walker_nav_menu extends Walker_Nav_Menu {
   
 // add classes to ul sub-menus
-function start_lvl( &$output, $depth = 0, $args = array() ) {
+function start_lvl( &$output, $depth ) {
     // depth dependent classes
     $indent = ( $depth > 0  ? str_repeat( "\t", $depth ) : '' ); // code indent
     $display_depth = ( $depth + 1); // because it counts the first submenu as 0
     $classes = array(
-        'sub-menu',
-        ( $display_depth % 2  ? 'menu-odd' : 'menu-even' ),
-        ( $display_depth >=2 ? 'sub-sub-menu' : '' ),
-        'menu-depth-' . $display_depth
+        'dropdown-menu'
         );
     $class_names = implode( ' ', $classes );
   
@@ -93,47 +90,55 @@ function start_lvl( &$output, $depth = 0, $args = array() ) {
 }
   
 // add main/sub classes to li's and links
- function start_el(  &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+ function start_el( &$output, $item, $depth, $args ) {
     global $wp_query;
     $indent = ( $depth > 0 ? str_repeat( "\t", $depth ) : '' ); // code indent
-  
+	  $class_names = $value = '';
+
+        //$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+
+        $class_names = in_array("current_page_item",$item->classes) ? ' active' : '';
+		$class_names1 = in_array("current-menu-ancestor",$item->classes) ? ' active' : '';
+        //$class_names1 = in_array("current_page_item",$item->menu_item_children->classes) ? ' active' : '';
     // depth dependent classes
     $depth_classes = array(
-        ( $depth == 0 ? 'main-menu-item' : 'sub-menu-item' ),
-        ( $depth >=2 ? 'sub-sub-menu-item' : '' ),
-        ( $depth % 2 ? 'menu-item-odd' : 'menu-item-even' ),
-        'menu-item-depth-' . $depth
+        ( $depth == 0 ? 'dropdown' : '' ),$class_names,$class_names1
     );
     $depth_class_names = esc_attr( implode( ' ', $depth_classes ) );
   
     // passed classes
-    $classes = empty( $item->classes ) ? array() : (array) $item->classes;
-    $class_names = esc_attr( implode( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) ) );
+    //$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+    //$class_names = esc_attr( implode( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) ) );
   
     // build html
-    $output .= $indent . '<li id="nav-menu-item-'. $item->ID . '" class="' . $depth_class_names . ' ' . $class_names . '">';
+    $output .= $indent . '<li id="" class="' . $depth_class_names .'">';
   
     // link attributes
     $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
     $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
     $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
     $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
-    $attributes .= ' class="menu-link ' . ( $depth > 0 ? 'sub-menu-link' : 'main-menu-link' ) . '"';
-  
-    $item_output = sprintf( '%1$s<a%2$s>%3$s%4$s%5$s</a>%6$s',
+	$attributes.='class="dropdown-toggle hvr-underline-from-center"';
+    //$attributes .= ' class="menu-link ' . ( $depth > 0 ? 'sub-menu-link' : 'main-menu-link' ) . '"';
+    $item_output = $args->before;
+        $item_output .= '<a'. $attributes .'>';
+		  $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+		  $item_output .= '</a>';
+        $item_output .= $args->after;
+   /* $item_output = sprintf( '%1$s<a%2$s>%3$s%4$s%5$s</a>%6$s',
         $args->before,
         $attributes,
         $args->link_before,
         apply_filters( 'the_title', $item->title, $item->ID ),
         $args->link_after,
         $args->after
-    );
+    );*/
+	
   
     // build html
     $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
 }
 }
-
 add_action( 'after_setup_theme', 'td_setup' );
 
 function td_setup() {
@@ -144,6 +149,7 @@ function td_setup() {
 if ( ! isset( $content_width ) ) {
 	$content_width = 474;
 }
+
 
 // menu end
 
