2e3ebcd33570aa9c3cf1cb85d572e3dc48abb693
Soumya Mondal <tysonchamp@users.noreply.github.com>
2015-12-14 15:49:12 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,33 @@
 <?php 
 // custom functions.php template for WordPress Theme Development
 
+// Mailchimp 3.0 API integration in wordpress
+
+function rudr_mailchimp_subscriber_status( $email, $status, $list_id, $api_key, $merge_fields = array('FNAME' => '','LNAME' => '') ){
+	$data = array(
+		'apikey'        => $api_key,
+    		'email_address' => $email,
+		'status'        => $status,
+		'merge_fields'  => $merge_fields
+	);
+	$mch_api = curl_init(); // initialize cURL connection
+ 
+	curl_setopt($mch_api, CURLOPT_URL, 'https://' . substr($api_key,strpos($api_key,'-')+1) . '.api.mailchimp.com/3.0/lists/' . $list_id . '/members/' . md5(strtolower($data['email_address'])));
+	curl_setopt($mch_api, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: Basic '.base64_encode( 'user:'.$api_key )));
+	curl_setopt($mch_api, CURLOPT_USERAGENT, 'PHP-MCAPI/2.0');
+	curl_setopt($mch_api, CURLOPT_RETURNTRANSFER, true); // return the API response
+	curl_setopt($mch_api, CURLOPT_CUSTOMREQUEST, 'PUT'); // method PUT
+	curl_setopt($mch_api, CURLOPT_TIMEOUT, 10);
+	curl_setopt($mch_api, CURLOPT_POST, true);
+	curl_setopt($mch_api, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($mch_api, CURLOPT_POSTFIELDS, json_encode($data) ); // send data in json
+ 
+	$result = curl_exec($mch_api);
+	return $result;
+}
+
+// End of Mailchimp API Function
+
 // enable featured image support for posts
 add_theme_support( 'post-thumbnails' ); 
  
