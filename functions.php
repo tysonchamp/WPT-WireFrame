3a51c031557f16505a125db70d8902da221b35c8
Soumya Mondal <tysonchamp@users.noreply.github.com>
2026-01-23 00:47:46 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@ function _remove_script_version( $src ){
 } 
 add_filter( 'script_loader_src', '_remove_script_version', 15, 1 ); 
 add_filter( 'style_loader_src', '_remove_script_version', 15, 1 );
+add_filter( 'wpcf7_autop_or_not', '__return_false' );
 
 
 // Advanced Custom fields section
